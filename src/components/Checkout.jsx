0da2fedac50551d82b83313a941518ad72cbc17a
Zachary F Jorgensen <jorgensenzachary00@gmail.com>
2024-04-05 17:32:04 -0700

    Created basic checkout form and added bootstrap styling

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,18 +1,76 @@
 export default function Checkout() {
   return (
     <div>
-      <form action="#">
-        <label htmlFor="name">First and Last Name</label>
-        <input id="name" type="text" placeholder="Name..." />
-        <label htmlFor="phone">Phone Number</label>
-        <input id="phone" type="number" placeholder="Phone Number..." />
-        <label htmlFor="creditcard cvv">Credit Card Info</label>
-        <input
-          id="creditcard"
-          type="number"
-          placeholder="Creditcard Number..."
-        />
-        <input id="cvv" type="number" placeholder="CVV..." />
+      <form action="#" className="container">
+        <div className="row g-3">
+          <label className="checkoutLabels" htmlFor="firstName lastName">
+            Name:
+          </label>
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="First name"
+              aria-label="First name"
+              id="firstName"
+            />
+          </div>
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Last name"
+              aria-label="Last name"
+              id="lastName"
+            />
+          </div>
+        </div>
+        <div className="row g-3">
+          <label className="checkoutLabels" htmlFor="phone email">
+            Contact:
+          </label>
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Phone Number"
+              aria-label="Phone Number"
+              id="phone"
+            />
+          </div>
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email"
+              aria-label="Email"
+              id="email"
+            />
+          </div>
+        </div>
+        <div className="row g-3">
+          <label className="checkoutLabels" htmlFor="credit cvv">
+            Payment Info:
+          </label>
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Credit Card Number"
+              aria-label="Credit Card Number"
+              id="credit"
+            />
+          </div>
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="CVV"
+              aria-label="CVV"
+              id="cvv"
+            />
+          </div>
+        </div>
       </form>
     </div>
   );
