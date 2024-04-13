748cc07dbf89e7d11f9f5c6ac5d5df7411c155dd
Zachary F Jorgensen <jorgensenzachary00@gmail.com>
2024-04-12 21:35:22 -0700

    removed old commented out code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,42 +43,5 @@ export default function Nav() {
         </div>
       </div>
     </div>
-    //   <div className="navbar">
-    //     <nav>
-    //       <h1>La Cantina</h1>
-    //       <h2>Est. 2024</h2>
-    //       <div className="dropdown">
-    //         <button
-    //           className="btn btn-secondary dropdown-toggle"
-    //           type="button"
-    //           data-bs-toggle="dropdown"
-    //           aria-expanded="false"
-    //         ></button>
-    //         <ul className="dropdown-menu dropdown-menu-end">
-    //           <li>
-    //             <a
-    //               className="dropdown-item"
-    //               href="#"
-    //               data-bs-toggle="modal"
-    //               data-bs-target="#staticBackdrop"
-    //             >
-    //               Start Order
-    //             </a>
-    //           </li>
-    //           <li>
-    //             <a className="dropdown-item" href="#">
-    //               Find Locations
-    //             </a>
-    //           </li>
-    //           <li>
-    //             <a className="dropdown-item" href="#">
-    //               Contact Us
-    //             </a>
-    //           </li>
-    //         </ul>
-    //       </div>
-    //     </nav>
-    //   </div>
-    // );
   );
 }
