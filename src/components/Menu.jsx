4893cb5ba54050fb39e056b9083b4ee0bd450adc
Zachary F Jorgensen <jorgensenzachary00@gmail.com>
2024-04-12 18:36:41 -0700

    fixed the responsiveness of the menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,93 +1,22 @@
-import { useState } from "react";
 import { menuItems } from "../MenuItems";
 
 export default function Menu(props) {
-  //   const [cart, setCart] = useState(0);
-  //   const [total, setTotal] = useState(0);
-
-  //   function addTotal(id) {
-  //     const selectedItem = menuItems.find((item) => item.id === id);
-  //     setTotal((prevTotal) => prevTotal + selectedItem.price);
-  //     setCart(cart + 1);
-  //   }
-
-  //   const menuItems = [
-  //     {
-  //       id: 1,
-  //       name: "California Burrito",
-  //       image:
-  //         "https://136667925.cdn6.editmysite.com/uploads/1/3/6/6/136667925/s962146695723897819_p235_i4_w1004.jpeg",
-  //       ingredients: ["Carne Asada", "French Fries", "Guacamole", "Sour Cream"],
-  //       price: 12.99,
-  //     },
-
-  //     {
-  //       id: 2,
-  //       name: "Bean and Cheese Burrito",
-  //       image:
-  //         "https://www.isabeleats.com/wp-content/uploads/2020/12/bean-cheese-burritos-small-13.jpg",
-  //       ingredients: ["Refried Beans", "Melted Cheese"],
-  //       price: 7.99,
-  //     },
-
-  //     {
-  //       id: 3,
-  //       name: "Tacos Dorados",
-  //       image:
-  //         "https://honest-food.net/wp-content/uploads/2019/03/tacos-dorados-500x500.jpg",
-  //       ingredients: [
-  //         "Fried Corn Tortilla",
-  //         "Shredded Beef",
-  //         "Guacamole",
-  //         "Pico De Gallo",
-  //       ],
-  //       price: 11.99,
-  //     },
-  //     {
-  //       id: 4,
-  //       name: "Fish Tacos",
-  //       image:
-  //         "https://kristineskitchenblog.com/wp-content/uploads/2023/05/fish-tacos-recipe-747-500x375.jpg",
-  //       ingredients: ["Talapia", "Fresh Cabbage", "Mango Habenaro Salsa"],
-  //       price: 10.99,
-  //     },
-  //     {
-  //       id: 5,
-  //       name: "Ceviche",
-  //       image:
-  //         "https://lifeatthetable.com/wp-content/uploads/2023/06/Life-At-The-Table-Ceviche.jpeg",
-  //       ingredients: ["Shrimp", "Pico De Gallo", "Lime", "Avocado", "Jalepeno"],
-  //       price: 13.99,
-  //     },
-  //     {
-  //       id: 6,
-  //       name: "Tres Leche Cake",
-  //       image:
-  //         "https://www.browneyedbaker.com/wp-content/uploads/2020/04/tres-leches-cake-11-square.jpg",
-  //       ingredients: ["Milk", "Cream", "Strawberries"],
-  //       price: 10.99,
-  //     },
-  //   ];
-
   const menuItemElements = menuItems.map((item) => {
     return (
-      <div className="row">
-        <div className="col-md-4">
-          <div className="card mb-3" key={item.id}>
-            <img src={item.image} className="card-img-top" alt={item.name} />
-            <div className="card-body">
-              <h5 className="card-title">{item.name}</h5>
-              <p className="card-text">
-                Ingredients: {item.ingredients.join(", ")}
-              </p>
-              <a
-                href="#"
-                className="btn btn-primary"
-                onClick={() => props.addTotal(item.id)}
-              >
-                {item.price}
-              </a>
-            </div>
+      <div className="col-md-12 mb-3" key={item.id}>
+        <div className="card">
+          <img src={item.image} className="card-img-top" alt={item.name} />
+          <div className="card-body">
+            <h5 className="card-title">{item.name}</h5>
+            <p className="card-text">
+              Ingredients: {item.ingredients.join(", ")}
+            </p>
+            <button
+              className="btn btn-primary"
+              onClick={() => props.addTotal(item.id)}
+            >
+              {item.price}
+            </button>
           </div>
         </div>
       </div>
